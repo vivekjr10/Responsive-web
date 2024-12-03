2a93ab58169b5aaea159e7ef41632d10bdb2e57e
vivekjr10 <vivekjr494@gmail.com>
2024-12-04 00:10:13 +0530

    first commit

---
+++ public class Main {.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class OddEvenChecker {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is odd or even
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
